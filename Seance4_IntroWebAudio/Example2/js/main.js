b3bfcf3196bf3fafd103a3e35fd6ea1181a94293
Mathis <mathis.andre@etu.univ-amu.fr>
2025-10-20 10:24:51 +0200

    example2 showing the waveform but trim bar not stored

--- Seance4_IntroWebAudio/Example2/js/main.js
+++ Seance4_IntroWebAudio/Example2/js/main.js
@@ -15,7 +15,22 @@ let ctx;
 
 const soundURL =
     'https://mainline.i3s.unice.fr/mooc/shoot2.mp3';
+
+const soundURLs = [
+   'https://upload.wikimedia.org/wikipedia/commons/a/a3/Hardstyle_kick.wav',
+   'https://upload.wikimedia.org/wikipedia/commons/transcoded/c/c7/Redoblante_de_marcha.ogg/Redoblante_de_marcha.ogg.mp3',
+   'https://upload.wikimedia.org/wikipedia/commons/transcoded/c/c9/Hi-Hat_Cerrado.ogg/Hi-Hat_Cerrado.ogg.mp3',
+   'https://upload.wikimedia.org/wikipedia/commons/transcoded/0/07/Hi-Hat_Abierto.ogg/Hi-Hat_Abierto.ogg.mp3',
+   'https://upload.wikimedia.org/wikipedia/commons/transcoded/3/3c/Tom_Agudo.ogg/Tom_Agudo.ogg.mp3',
+   'https://upload.wikimedia.org/wikipedia/commons/transcoded/a/a4/Tom_Medio.ogg/Tom_Medio.ogg.mp3',
+   'https://upload.wikimedia.org/wikipedia/commons/transcoded/8/8d/Tom_Grave.ogg/Tom_Grave.ogg.mp3',
+   'https://upload.wikimedia.org/wikipedia/commons/transcoded/6/68/Crash.ogg/Crash.ogg.mp3',
+   'https://upload.wikimedia.org/wikipedia/commons/transcoded/2/24/Ride.ogg/Ride.ogg.mp3'
+]
+
 let decodedSound;
+let decodedSounds = [];
+
 
 let canvas, canvasOverlay;
 // waveform drawer is for drawing the waveform in the canvas
@@ -33,6 +48,7 @@ window.onload = async function init() {
 
     // two canvas : one for drawing the waveform, the other for the trim bars
     canvas = document.querySelector("#myCanvas");
+    const context = canvas.getContext('2d');
     canvasOverlay = document.querySelector("#myCanvasOverlay");
 
     // create the waveform drawer and the trimbars drawer
@@ -45,8 +61,35 @@ window.onload = async function init() {
     // Note that we cannot use await outside an async function
     // so we had to declare the init function as async
     decodedSound = await loadAndDecodeSound(soundURL, ctx);
-    waveformDrawer.init(decodedSound, canvas, '#83E83E');
-    waveformDrawer.drawWave(0, canvas.height);
+
+    let promises = soundURLs.map(url => loadAndDecodeSound(url, ctx));
+    decodedSounds = await Promise.all(promises);
+
+    let buttonsContainer = document.querySelector("#buttons");
+
+
+    decodedSounds.forEach((element, index) => {
+        let button = document.createElement("button");
+        button.id = "playButton"
+        button.textContent = `Play sound ${index + 1}`;
+        buttonsContainer.appendChild(button);
+        button.onclick = function() {
+            let start = pixelToSeconds(trimbarsDrawer.leftTrimBar.x, decodedSound.duration, canvas.width);
+            let end = pixelToSeconds(trimbarsDrawer.rightTrimBar.x, decodedSound.duration, canvas.width);
+            console.log("start: " + start + " end: " + end);
+            // from utils.js
+            playSound(ctx, decodedSound, start, end);
+            // playSound(ctx, element, 0, element.duration);
+            context.clearRect(0, 0, canvas.width, canvas.height);
+            waveformDrawer.init(element, canvas, '#83E83E');
+            waveformDrawer.drawWave(0, canvas.height);
+        };
+        button.disabled = false;
+    });
+
+
+    // waveformDrawer.init(decodedSound, canvas, '#83E83E');
+    // waveformDrawer.drawWave(0, canvas.height);
 
     // we enable the play sound button, now that the sound is loaded and decoded
     playButton.disabled = false;
